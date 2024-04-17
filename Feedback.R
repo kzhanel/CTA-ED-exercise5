5e60c0898282d88c5fbc141dfb3ad4429e0f48ed
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-17 14:16:20 +0100

    feedback

---
+++ Feedback.R
@@ -0,0 +1,23 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+# very good and very well explained
+
+# question 2
+# -----------------------------------------------------------
+# thoughtful and well explained analysis
+
+# question 3
+# -----------------------------------------------------------
+# you didn't really interpret the output of the validation: 
+# would have been worth checking the PreText documentation to figure out what to make of the results. 
+
+# overall code quality: 
+# -----------------------------------------------------------
+# excellent and well commented
+# in future do not include earlier versions of the code that didn't work. You can simply mention that you tried to run things a different way unsuccessfully.
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# excellent. Thoughtful and meticulous, except for the PreText part, which lost you a little I think. 
